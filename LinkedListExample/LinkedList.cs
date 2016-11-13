284d21dcac28bb24c5277d21ce015b16561a7fd9
ViacheslavLisianskii <ton.fier@gmail.com>
2016-11-13 18:05:53 +0300

    Linked List

---
+++ LinkedListExample/LinkedList.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+
+namespace LinkedListExample
+{
+    public class LinkedList
+    {
+        private Node<int> _head;
+        
+        public IEnumerable<int> Nodes
+        {
+            get
+            {
+                var temp = _head;
+                while (temp != null)
+                {
+                    yield return temp.Data;
+                    temp = temp.Next;
+                }
+            }
+        }
+
+        public void Add(int value)
+        {
+            var newNode = new Node<int>(value);
+            if (_head == null || _head.Data >= newNode.Data)
+            {
+                newNode.Next = _head;
+                _head = newNode;
+            }
+            else
+            {
+                var current = _head;
+                while (current.Next != null && current.Next.Data < newNode.Data)
+                {
+                    current = current.Next;
+                }
+
+                newNode.Next = current.Next;
+                current.Next = newNode;
+            }
+        }
+
+        public void AddRange(params int[] values)
+        {
+            foreach (var value in values)
+            {
+                Add(value);
+            }
+        }
+    }
+
+    public class Node<T>
+    {
+        public T Data;
+        public Node<T> Next;
+
+        public Node(T d)
+        {
+            Data = d;
+            Next = null;
+        }
+    }
+}
---
+++ LinkedListExample/SortOrderTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LinkedListExample
+{
+    [TestClass]
+    public class SortOrderTests
+    {
+        [TestMethod]
+        public void EmptyList()
+        {
+            // arrange
+            var linkedList = new LinkedList();
+
+            // assert
+            Assert.AreEqual(0, linkedList.Nodes.Count());
+        }
+
+        [TestMethod]
+        public void CanAddElement()
+        {
+            // arrange
+            var linkedList = new LinkedList();
+            const int value = 13;
+            var unsortedList = new List<int> { value };
+
+            // act
+            linkedList.AddRange(unsortedList.ToArray());
+
+            // assert
+            Assert.AreEqual(1, linkedList.Nodes.Count());
+            Assert.AreEqual(value, linkedList.Nodes.FirstOrDefault());
+        }
+
+        [TestMethod]
+        public void CanHaveDuplicates()
+        {
+            // arrange
+            var linkedList = new LinkedList();
+            var unsortedList = new List<int> { 1, 1 };
+
+            // act
+            linkedList.AddRange(unsortedList.ToArray());
+
+            // assert
+            Assert.AreEqual(2, linkedList.Nodes.Count());
+        }
+        
+        [TestMethod]
+        public void SortOrderOfAnyNumbers()
+        {
+            // arrange
+            var linkedList = new LinkedList();
+            var unsortedList = new List<int> { -1, -8, 10, 5, 7 };
+
+            // act
+            linkedList.AddRange(unsortedList.ToArray());
+            var items = linkedList.Nodes;
+            
+            // assert
+            Assert.IsTrue(items.SequenceEqual(unsortedList.OrderBy(i => i)));
+        }
+    }
+}
